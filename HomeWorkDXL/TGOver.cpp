91afbe9532de5ed28001ca1c8165f55c5b665cdf
大和 松永 <yama.8010.pon@icloud.com>
2025-12-15 23:18:06 +0900

    コミット

--- HomeWorkDXL/TGOver.cpp
+++
@@ -1,7 +0,0 @@
-#include "TGInclude.h"
-void Over(System* system)
-{
-    SetFontSize(40);
-    DrawString(WIDTH / 2 - 40 / 2 * 9 / 2, HEIGHT / 3, "GAME OVER", 0xFF0000);
-    if (system->timer > 60 * 5) system->scene = TITLE;
-}
